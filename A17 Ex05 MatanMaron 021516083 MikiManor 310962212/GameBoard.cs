d327555bad99a8da972bf5715336641c53356881
matan_m <matan_m@MATANM-W7-PC>
2017-01-31 13:11:06 +0200

    11

--- A17 Ex05 MatanMaron 021516083 MikiManor 310962212/GameBoard.cs
+++ A17 Ex05 MatanMaron 021516083 MikiManor 310962212/GameBoard.cs
@@ -13,24 +13,32 @@ namespace Ex05_Othelo
 
         private void InitializeComponent()
         {
+            const int k_Size = 60;
+            const int k_Space = k_Size+10;
             this.MaximizeBox = false;
             this.ShowIcon = false;
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
 
-            List<PictureBox> PictureList = new List<PictureBox>();
+            List<Button> buttonOnBoardList = new List<Button>();
             for (int i = 0; i < OtheloBoard.BoardSize; i++)
             {
                 for (int j = 0; j < OtheloBoard.BoardSize; j++)
                 {
-                    PictureBox picture = new PictureBox();
-                    picture.Size = new System.Drawing.Size(20, 20);
-                    picture.Top = (i * 20 + 2);
-                    picture.Left = (j * 20 + 2);
-                    picture.BackColor = System.Drawing.Color.Green;
-                    this.Controls.Add(picture);
-                    PictureList.Add(picture);
+                    Button buttonOnBoard = new Button();
+                    buttonOnBoard.Size = new System.Drawing.Size(k_Size, k_Size);
+                    if (i == 0 || j == 0)
+                    {
+                        buttonOnBoard.Location = new System.Drawing.Point(40, 40);
+                    }
+                    buttonOnBoard.Location = new System.Drawing.Point(i*k_Size+40, j * k_Size+40);
+                    buttonOnBoard.BackColor = System.Drawing.Color.ForestGreen;
+                    this.Controls.Add(buttonOnBoard);
+                    buttonOnBoardList.Add(buttonOnBoard);
                 }
             }
+            this.StartPosition = FormStartPosition.CenterParent;
+            this.BackColor = System.Drawing.Color.Green;
+            this.Size = new System.Drawing.Size(k_Space * OtheloBoard.BoardSize + 40, k_Space * OtheloBoard.BoardSize + 60);
         }
     }
 }
